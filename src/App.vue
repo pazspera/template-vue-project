69c88b596df6e7c8852dca0028dcf960d2501620
Paz Spera <mspera03@gmail.com>
2026-06-12 01:43:09 -0300

    style: Definidos pares de tipografías para 6 estilos. Agregados ejemplos en App

--- src/App.vue
+++ src/App.vue
@@ -10,38 +10,38 @@
       </p>
     
       <section class="theme-one">
-        <h2>Theme 1: To be named</h2>
-        <p>Title: Fjalla One</p>
+        <p>Theme 1: To be named</p>
+        <h2>Title: Fjalla One</h2>
         <p>Text: Libre Baskerville</p>
       </section>
   
       <section class="theme-two">
-        <h2>Theme 2</h2>
-        <p>Title: Yeseva One</p>
+        <p>Theme 2</p>
+        <h2>Title: Yeseva One</h2>
         <p>Text: Josefin Sans</p>
       </section>
 
        <section class="theme-three">
-        <h2>Theme 3</h2>
-        <p>Title: Spectral</p>
+        <p>Theme 3</p>
+        <h2>Title: Spectral</h2>
         <p>Text: Karla</p>
       </section>
 
       <section class="theme-four">
-        <h2>Theme 4</h2>
-        <p>Title: Ubuntu Bold</p>
+        <p>Theme 4</p>
+        <h2>Title: Ubuntu Bold</h2>
         <p>Text: Open Sans</p>
       </section>
 
       <section class="theme-five">
-        <h2>Theme 5</h2>
-        <p>Title: Ultra</p>
+        <p>Theme 5</p>
+        <h2>Title: Ultra</h2>
         <p>Text: Slabo 27px</p>
       </section>
 
       <section class="theme-six">
-        <h2>Theme 6</h2>
-        <p>Title: Cabin</p>
+        <p>Theme 6</p>
+        <h2>Title: Cabin</h2>
         <p>Text: Roboto Contensed</p>
       </section>
     </div>
