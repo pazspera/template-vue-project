b52f9033ccc455db1980423c3475d00e07e86ae5
Paz Spera <mspera03@gmail.com>
2026-06-12 23:33:55 -0300

    feature: Estructura componente ThemeCard. Continuar desarollo cuando estén migrados los componentes de tipografía.

---
+++ src/components/ThemeCard.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+    <section class="theme-card">
+        <div class="grid grid-cols-12">
+            <div class="col-span-12 gap-4">
+                
+            </div>
+        </div>
+    </section>
+</template>
